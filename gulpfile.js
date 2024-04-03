63bfb7db0baf5fe4f9d19a166c8338ee964740d1
Diogo <diogo-sos@outlook.com>
2024-04-03 16:07:46 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,14 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function pictures(){
+    return gulp.src('./src/pictures/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/pictures'));
+}
+
+exports.default = gulp.parallel(styles,pictures);
+
 exports.watch = function (){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
 }
